b005a093784012acd16dc6ab9745821343aee62b
Skeletonprey <78114563+Skeletonprey@users.noreply.github.com>
2021-06-18 22:00:47 -0700

    Removed tabs

--- script.js
+++
@@ -1,6 +0,0 @@
-const tags = document.querySelectorAll('[data-tab-target]')
-
-tabs.forEach('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
-    target.classlist.add('active')
-})
